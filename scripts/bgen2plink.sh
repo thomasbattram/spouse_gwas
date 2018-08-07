a4a0cba17113ec7a7abdb56a2ad5efe7746cdcf3
Thomas Battram <tb13101@bristol.ac.uk>
2018-08-07 15:12:57 +0100

    changed directory and file names

--- scripts/bgen2plink.sh
+++ scripts/bgen2plink.sh
@@ -5,14 +5,14 @@ bgen_pattern=/panfs/panasas01/dedicated-mrcieu/research/data/ukbiobank/_latest/U
 #bgi file
 bgen_index_pattern=/panfs/panasas01/dedicated-mrcieu/research/data/ukbiobank/_latest/UKBIOBANK_Array_Genotypes_500k_HRC_Imputation/data/raw_downloaded/dosage_bgen/ukb_bgi_chrCHROM_v2.bgi
 #List of SNPs you want to extract
-snp_list=/panfs/panasas01/sscm/lh14833/Assortative/heightSNPs.txt
+snp_list=~/spouse_gwas/data/heightSNPs.txt
 
 # Extract using bgen to temporary files
 temp_prefix=temp_genotypes
 for chrom in {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22}; do
     inbgen=${bgen_pattern/CHROM/$chrom}
     inbgenidx=${bgen_index_pattern/CHROM/$chrom}
-    /panfs/panasas01/sscm/lh14833/gavinband-bgen-798eca81c0fa/build/apps/bgenix -g $inbgen -i $inbgenidx -incl-rsids $snp_list -v11  > $temp_prefix.$chrom.bgen
+    ~/programs/gavinband-bgen-0b7a2803adb5/build/apps/bgenix -g $inbgen -i $inbgenidx -incl-rsids $snp_list -v11 > $temp_prefix.$chrom.bgen
 done
 
 #Use QCtools to convert to plink format
@@ -27,10 +27,10 @@ done
 
 
 #Also will need the idlist to link the IDs to the genotype files, they are in the same order so just paste across and then remove columns in R or unix 
-paste ~/Assortative/idlist file.fam > linkedfile.fam
+paste ~/spouse_gwas/idlist file.fam > linkedfile.fam
 
 R
-input<-read.table("~/Assortative/linkedHeight.SNPs.fam")
-output<-input[c(1,1,4:7)]
+input <- read.table("~/spouse_gwas/linked_height_SNPs.fam")
+output <- input[c(1, 1, 4:7)]
 
-write.table(output, "~/Assortative/Height.SNPs.fam", sep=" ", quote=F, row.names=F, col.names=F)
+write.table(output, "~/spouse_gwas/height_SNPs.fam", sep=" ", quote=F, row.names=F, col.names=F)
