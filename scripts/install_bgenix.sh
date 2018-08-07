dfc92b6576aea8ef6c0e225a47bdcf009886f9ba
Thomas Battram <tb13101@bristol.ac.uk>
2018-08-07 15:13:37 +0100

    near exact copy of Kaitlin's code to install bgenix

---
+++ scripts/install_bgenix.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get and extract
+wget http://bitbucket.org/gavinband/bgen/get/master.tar.gz
+tar -zxvf master.tar.gz
+
+# Configure and build
+cd gavinband-bgen-d1f03a2c308a
+./waf-1.8.13 configure
+./waf-1.8.13
+
+# bgenix will be installed in build/apps/bgenix
+
+# If you have ~/bin in your $PATH then add bgenix to ~/bin, e.g.
+cd ~/bin
+ln -s ~/programs/gavinband-bgen-d1f03a2c308a/build/apps/bgenix
+
