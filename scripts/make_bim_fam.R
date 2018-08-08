11f9e9c32fb145b8c09d9f4ad7b5bca3c10b6df5
Thomas Battram <tb13101@bristol.ac.uk>
2018-08-08 20:47:14 +0100

    Removed making bim and changed fam - previously had to make from scratch as using older version of qctools for the bgen to bed conversion

--- scripts/make_bim_fam.R
+++ scripts/make_bim_fam.R
@@ -1,5 +1,5 @@
 # ------------------------------------------------
-# Making a fam and bim file for the spouse pairs in biobank
+# Making a fam file 
 # ------------------------------------------------
 
 rm(list = ls())
@@ -11,15 +11,6 @@ lapply(pkgs, require, character.only = T)
 
 devtools::load_all("~/repos/usefunc/")
 
-pairs <- read_delim("data/spouse_pairs.txt", delim = " ")
-snps <- read_delim("data/height_mr_dataset.txt", delim = " ")
-snps2 <- read_delim("data/snp_pos.txt", delim = "\t", col_names = F)
-colnames(snps2) <- c("CHR", "SNP", "GD", "POS", "A1", "A2")
-
-# ------------------------------------------------
-# Make fam file
-# ------------------------------------------------
-
 # FAM format
 # FID IID PID MID SEX PHENO
 # --  --  --  --  --  ---
@@ -27,38 +18,13 @@ colnames(snps2) <- c("CHR", "SNP", "GD", "POS", "A1", "A2")
 # --  --  --  --  --  ---
 # --  --  --  --  --  ---
 
-fam <- pairs %>%
-	mutate(FID = Couple) %>%
-	mutate(IID = genID) %>%
-	mutate(PID = 0) %>%
-	mutate(MID = 0) %>%
-	mutate(SEX = as.factor(Sex)) %>%
-	mutate(PHENO = 0) %>%
-	dplyr::select(FID, IID, PID, MID, SEX, PHENO)
+fam <- read_delim("data/binary/merged_dat.fam", delim = " ", col_names = F)
+colnames(fam) <- c("FID", "IID", "PID", "MID", "SEX", "PHENO")
 
+linker <- read_delim("data/idlist", delim = " ", col_names = F)
 
-# ------------------------------------------------
-# Make bim file
-# ------------------------------------------------
+fam[,c(1,2)] <- linker[[1]]
 
-# BIM format
-# CHR SNP/CPG GD POS A1 A2
-# --	--    -- --  -- --
-# --	--    -- --  -- --
-# --	--    -- --  -- --
-# --	--    -- --  -- --
+write.table(fam, "data/binary/merged_dat.fam", col.names = F, row.names = F, quote = F)
 
-bim <- snps %>%
-	left_join(snps2) %>%
-	mutate(A1 = effect_allele.exposure) %>%
-	mutate(A2 = other_allele.exposure) %>%
-	dplyr::select(CHR, SNP, GD, POS, A1, A2)
-
-for (i in 1:22) {
-	temp_bim <- bim %>%
-		dplyr::filter(CHR == i)
-
-	write.table(temp_bim, file = paste0("data/binary/temp_SNPs.", i, ".bim"), quote = F, col.names = F, row.names = F)
-	write.table(fam, file = paste0("data/binary/temp_SNPs.", i, ".fam"), quote = F, col.names = F, row.names = F)
-}
 
