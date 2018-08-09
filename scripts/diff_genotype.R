3a3a2737dd9e67e015ab04182511a51a0c08e59d
Thomas Battram <tb13101@bristol.ac.uk>
2018-08-09 15:14:26 +0100

    Started script to make the difference in genotype file

---
+++ scripts/diff_genotype.R
@@ -0,0 +1,75 @@
+# ------------------------------------------------
+# generating the genotype for gwas 
+# ------------------------------------------------
+
+rm(list = ls())
+
+setwd("~/spouse_gwas")
+
+pkgs <- c("tidyverse")
+lapply(pkgs, require, character.only = T)
+
+devtools::load_all("~/repos/usefunc/")
+
+dat <- read.table("data/merged_dat_recode.raw", header = T)
+pairs <- read.table("data/spouse_pairs.txt", header = T)
+
+# ------------------------------------------------
+# make genotype file
+# ------------------------------------------------
+
+res <- pairs %>%
+	left_join(dat, by = c("genID" = "FID"))
+str(res)
+
+snps <- grep("rs[0-9]", colnames(res), value = T)
+
+test <- res[1:20,]
+
+dupval <- duplicated(test$Couple)
+
+test_c1 <- test[dupval,]
+colnames(test_c1[snps]) <- paste0(colnames(test_c1[snps]), "_c1")
+test_c2 <- test[!dupval,]
+colnames(test_c2[snps]) <- paste0(colnames(test_c2[snps]), "_c2")
+stopifnot(test_c1$Couple == test_c2$Couple)
+
+test_diff <- test[dupval,]
+colnames(test_diff[snps]) <- paste0(colnames(test_diff[snps]), "_diff")
+
+test_diff[, -c(1:11)] <- test_c1[,-c(1:11)] - test_c2[,-c(1:11)]
+colnames(test_diff[, -c(1:11)]) <- paste0(colnames(test_diff[, -c(1:11)]), "_diff")
+colnames(test_diff)
+# START FROM HERE!!!!!!
+
+
+
+fin_dat <- list()
+i=1
+for (i in 1:length(snps)) {
+	exlude_snp <- snps[-i]
+	temp <- test %>%
+		dplyr::select(-one_of(exlude_snp)) %>%
+		spread(Couple)
+}
+
+test <- res %>%
+	gather(variable, value, -(phenID:PHENOTYPE)) %>%
+	unite(temp, Couple, variable) %>%
+	spread(temp, value)
+
+test[1:10, 1:20]
+dim(test)
+head(test)
+
+
+df <- data.frame(month=rep(1:3,2),
+                 student=rep(c("Amy", "Bob"), each=3),
+                 A=c(9, 7, 6, 8, 6, 9),
+                 B=c(6, 7, 8, 5, 6, 7))
+
+
+df %>% 
+  gather(variable, value, -(month:student)) %>%
+  unite(temp, student, variable) %>%
+  spread(temp, value)
