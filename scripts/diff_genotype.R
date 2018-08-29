a32373de579136daee8a737f2cc4d4a860170eda
Thomas Battram <tb13101@bristol.ac.uk>
2018-08-29 15:51:47 +0100

    Added code to create couple differences file

--- scripts/diff_genotype.R
+++ scripts/diff_genotype.R
@@ -12,7 +12,8 @@ lapply(pkgs, require, character.only = T)
 devtools::load_all("~/repos/usefunc/")
 
 dat <- read.table("data/merged_dat_recode.raw", header = T)
-pairs <- read.table("data/spouse_pairs.txt", header = T)
+pairs <- read.table("data/spouse_pairs_height_chd.txt", header = T)
+bim <- read_bim("data/binary/merged_dat")
 
 # ------------------------------------------------
 # make genotype file
@@ -24,56 +25,38 @@ str(res)
 
 snps <- grep("rs[0-9]", colnames(res), value = T)
 
-test <- res[1:20, ] %>%
+gen <- res %>%
+	arrange(Couple) %>%
 	arrange(Sex) # arranging by sex ensures when looking at duplicates you pick out all males/females
-
-dupval <- duplicated(test$Couple) # duplicated values = couples
+head(gen[, 1:15])
+dupval <- duplicated(gen$Couple) # duplicated values = couples
 
 # Extract one from each couple
-test_c1 <- test[dupval, ] 
-colnames(test_c1[snps]) <- paste0(colnames(test_c1[snps]), "_c1")
-test_c2 <- test[!dupval, ]
-colnames(test_c2[snps]) <- paste0(colnames(test_c2[snps]), "_c2")
-stopifnot(test_c1$Couple == test_c2$Couple)
+gen_c1 <- gen[dupval, ] 
+colnames(gen_c1[snps]) <- paste0(colnames(gen_c1[snps]), "_c1")
+gen_c2 <- gen[!dupval, ]
+colnames(gen_c2[snps]) <- paste0(colnames(gen_c2[snps]), "_c2")
+stopifnot(gen_c1$Couple == gen_c2$Couple)
 
-test_diff <- test[dupval, ]
-colnames(test_diff[snps]) <- paste0(colnames(test_diff[snps]), "_diff")
+gen_diff <- gen[dupval, ]
+colnames(gen_diff[snps]) <- paste0(colnames(gen_diff[snps]), "_diff")
 
-# Minus test couple 1 from test couple 2
-test_diff[, -c(1:11)] <- test_c1[,-c(1:11)] - test_c2[,-c(1:11)]
-colnames(test_diff[, -c(1:11)]) <- paste0(colnames(test_diff[, -c(1:11)]), "_diff")
-colnames(test_diff)
-# START FROM HERE!!!!!!
-summary(test_diff[,12:20])
-test_diff[, -c(1:11)] <- 
+# Minus gen couple 1 from gen couple 2
+gen_diff[, -c(1:13)] <- gen_c1[, -c(1:13)] - gen_c2[, -c(1:13)]
+colnames(gen_diff[, -c(1:13)]) <- paste0(colnames(gen_diff[, -c(1:13)]), "_diff")
+colnames(gen_diff)
 
+summary(gen_diff[, 14:20])
+# what to do with NAs???
 
-fin_dat <- list()
-i=1
-for (i in 1:length(snps)) {
-	exlude_snp <- snps[-i]
-	temp <- test %>%
-		dplyr::select(-one_of(exlude_snp)) %>%
-		spread(Couple)
-}
+# ------------------------------------------------
+# make final difference file
+# ------------------------------------------------
+fin_dat <- gen_diff %>%
+	mutate(height_diff = gen_c1[, "Height"] - gen_c2[, "Height"]) %>%
+	mutate(age_diff = gen_c1[, "DoB"] - gen_c2[, "DoB"]) %>%
+	mutate(chd_diff = gen_c1[, "chd"] - gen_c2[, "chd"])
 
-test <- res %>%
-	gather(variable, value, -(phenID:PHENOTYPE)) %>%
-	unite(temp, Couple, variable) %>%
-	spread(temp, value)
-
-test[1:10, 1:20]
-dim(test)
-head(test)
+write.table(fin_dat, file = "data/differences_dat.txt", qu = F, col = T, row = F, sep = "\t")
 
 
-df <- data.frame(month=rep(1:3,2),
-                 student=rep(c("Amy", "Bob"), each=3),
-                 A=c(9, 7, 6, 8, 6, 9),
-                 B=c(6, 7, 8, 5, 6, 7))
-
-
-df %>% 
-  gather(variable, value, -(month:student)) %>%
-  unite(temp, student, variable) %>%
-  spread(temp, value)
