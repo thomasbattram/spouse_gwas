cabb5e5861b9d95f78715fabe301e9fbe0a92878
Thomas Battram <tb13101@bristol.ac.uk>
2018-09-10 22:05:43 +0100

    Made simple to do list for the mr analysis script

---
+++ scripts/mr_analysis.R
@@ -0,0 +1,9 @@
+# ------------------------------------------------
+# Within spouse MR analyses
+# ------------------------------------------------
+
+# 1. Read in both exposure and outcome gwas dat
+# 2. Run the MR analyses - sensitivity analyses? 
+# 3. Read in phenos from MR base
+# 4. Estimates from step 2 with estimates from step 3
+# 5. Party 
