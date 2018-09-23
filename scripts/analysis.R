b36d5f3fc2856075203d6cdb8b66acbb4cf35a1f
Thomas Battram <tb13101@bristol.ac.uk>
2018-09-23 18:34:48 +0100

    made script more modular

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -9,12 +9,12 @@ pkgs <- c("tidyverse", "conflicted", "ggExtra")
 lapply(pkgs, require, character.only = T)
 
 devtools::load_all("~/repos/usefunc/")
-trait <- "height"
-dat <- read_delim(paste0("data/differences_dat_", trait, ".txt"), delim = "\t")
+traits <- c("chd", "height")
+dat <- read_delim(paste0("data/differences_dat_", paste(traits, collapse = "_"), ".txt"), delim = "\t")
 
 snps <- grep("rs[0-9]", colnames(dat), value = T)
 
-# regress Height on SNPs
+# regress height on SNPs
 i=snps[1]
 comp_res <- list()
 sum_res <- list()
@@ -24,20 +24,36 @@ vars[vars == 0] # rs9825951_A has no variance - removing
 
 dat <- dplyr::select(dat, -one_of(names(vars[vars==0])))
 snps <- grep("rs[0-9]", colnames(dat), value = T)
-for (i in snps) {
-	fom <- as.formula(paste0("trait_diff ~ ", i, " + age_diff + Sex"))
-	temp <- lm(fom, data = dat)
-	x <- summarise_lm(temp, trait, i)
-	comp_res[[i]] <- x
-	sum_res[[i]] <- x$summary_dat	
+i=snps[1]
+j=traits[1]
+comp_res2 <- list()
+sum_res2 <- list()
+for (j in traits) {
+	for (i in snps) {
+		print(j)
+		fom <- as.formula(paste0(j, "_diff ~ ", i, " + age_diff + Sex"))
+		temp <- lm(fom, data = dat)
+		x <- summarise_lm(temp, j, i)
+		comp_res[[i]] <- x
+		sum_res[[i]] <- x$summary_dat
+	}
+	temp <- do.call(rbind, comp_res)
+	temp <- as.data.frame(temp) %>%
+		rownames_to_column(var = "snp") %>%
+		mutate(snp = gsub("_[ACTG]", "", snp))
+	comp_res2[[j]] <- temp
+
+	temp <- do.call(rbind, sum_res)
+	temp <- as.data.frame(temp) %>%
+		rownames_to_column(var = "snp") %>%
+		mutate(snp = gsub("_[ACTG]", "", snp))
+	sum_res2[[j]] <- temp
 }
 
-res <- do.call(rbind, sum_res)
-res <- as.data.frame(res) %>%
-	rownames_to_column(var = "snp") %>%
-	mutate(snp = gsub("_[ACTG]", "", snp))
+res <- do.call(rbind, sum_res2)
+rownames(res) <- NULL
 
-write.table(res, file = paste0("data/", trait, "_spouse_diff_gwas_res.txt"), col.names = T, row.names = F, qu = F, sep = "\t")
+write.table(res, file = paste0("data/", paste(traits, collapse = "_"), "_spouse_diff_gwas_res.txt"), col.names = T, row.names = F, qu = F, sep = "\t")
 
 ################################################
 ##
