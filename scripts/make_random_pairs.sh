16741e93a9943c560c4806c56ddf920b6f6c3f65
Thomas Battram <tb13101@bristol.ac.uk>
2018-09-25 10:31:29 +0100

    minor changes

--- scripts/make_random_pairs.sh
+++ scripts/make_random_pairs.sh
@@ -2,12 +2,12 @@
 
 cd ~/spouse_gwas/
 
-traits="height"
+traits="chd,height"
 
 for i in {1..100}
 do
 	echo $i
-	Rscript scripts/random_non_spouse_pairs.R $i $traits ""
+	Rscript scripts/random_non_spouse_pairs.R $i $traits "chd"
 	echo "Finished $i"
 done 
 
